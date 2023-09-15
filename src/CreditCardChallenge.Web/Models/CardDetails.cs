ec8f6efe0aa273be3d427a6b5fc9f742feac7d5a
Fredrick Amoako <fredrick.amoako@amalitech.com>
2023-09-15 08:27:36 +0000

    feat: add card component

---
+++ src/CreditCardChallenge.Web/Models/CardDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CreditCardChallenge.Web.Models;
+
+public class CardDetails
+{
+    [CreditCard]
+    public string CardNumber { get; set; }
+    [Required]
+    public string CardName { get; set; }
+    [StringLength(2)]
+    public string Month { get; set; }
+    [StringLength(2)]
+    public string Year { get; set; }
+    [StringLength(4)]
+    public string Cvv { get; set; }
+}
