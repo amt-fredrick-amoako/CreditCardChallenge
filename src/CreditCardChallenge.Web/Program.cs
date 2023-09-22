4138d91dcb8e410029b76f0748ad683ba4f55a50
Fredrick Amoako <105827771+amt-fredrick-amoako@users.noreply.github.com>
2023-09-22 14:44:41 +0000

    Revert "Fix/UI animations"

--- src/CreditCardChallenge.Web/Program.cs
+++ src/CreditCardChallenge.Web/Program.cs
@@ -1,6 +1,4 @@
 using CreditCardChallenge.Web;
-using CreditCardChallenge.Web.Models;
-using FluentValidation;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,6 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddValidatorsFromAssemblyContaining<CardDetailsValidator>(ServiceLifetime.Transient);
 
 await builder.Build().RunAsync();
